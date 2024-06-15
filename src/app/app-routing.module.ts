63e0c4f2db4bc0777cef68defcca374f871e5c2f
NicolasSM17 <segoviamatosjesunicolas@gmail.com>
2024-06-15 06:45:19 -0500

    subida a github, paginas principales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ListaEventosComponent } from './pages/lista-eventos/lista-eventos.component';
+import { DetalleEventoComponent } from './pages/detalle-evento/detalle-evento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'listadoEventos', component: ListaEventosComponent},
+  {path: 'detalleEvento', component: DetalleEventoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ListaEventosComponent } from './pages/lista-eventos/lista-eventos.component';
+import { DetalleEventoComponent } from './pages/detalle-evento/detalle-evento.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListaEventosComponent,
+    DetalleEventoComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
